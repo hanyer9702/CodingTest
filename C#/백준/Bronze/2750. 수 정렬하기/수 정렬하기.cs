d1be45e1aec09ab3191d86d44b70f680f9c91600
hanyer9702 <56014935+hanyer9702@users.noreply.github.com>
2026-03-25 15:35:03 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 5896 KB -BaekjoonHub

--- C#/백준/Bronze/2750. 수 정렬하기/수 정렬하기.cs
+++ C#/백준/Bronze/2750. 수 정렬하기/수 정렬하기.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
@@ -13,7 +13,18 @@ class Program
             list.Add(int.Parse(Console.ReadLine()));
         }
         
-        list.Sort();
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = 0; j < n - i - 1 ; j++)
+            {
+                if(list[j] > list[j + 1])
+                {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
         
         foreach(var num in list)
         {
